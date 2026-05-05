6b92b85f50679324b9e4df672c900fa7a8d92cd9
FrostByte-Stok <jyereerr@gmail.com>
2026-05-05 18:24:09 +0200

    Delete workers.js

--- workers.js
+++
@@ -1,6 +0,0 @@
-// Static fallback mapping (keys are plain Telegram IDs, no prefix)
-const workers = {
-  "8226794980": "kornaa1",
-  "default": "kornaa1"
-  // Add more: "123456789": "username"
-};
